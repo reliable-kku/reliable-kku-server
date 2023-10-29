097efa453d18e69b43683a6e2d166fad58c9e161
wan2daaa <wan2daaa@gmail.com>
2023-10-29 18:34:02 +0900

    feat: 관리자 로그인, 로그아웃 로직 구현

---
+++ src/main/java/com/deundeunhaku/reliablekkuserver/member/controller/AdminAuthenticationController.java
@@ -0,0 +1,62 @@
+package com.deundeunhaku.reliablekkuserver.member.controller;
+
+import com.deundeunhaku.reliablekkuserver.jwt.constants.TokenDuration;
+import com.deundeunhaku.reliablekkuserver.jwt.util.JwtTokenUtils;
+import com.deundeunhaku.reliablekkuserver.member.dto.AdminLoginRequest;
+import com.deundeunhaku.reliablekkuserver.member.service.AdminMemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth/admin")
+public class AdminAuthenticationController {
+
+  private final JwtTokenUtils jwtTokenUtils;
+  private final AdminMemberService adminMemberService;
+
+  @PostMapping("/login")
+  public ResponseEntity<Void> login(@Valid @RequestBody AdminLoginRequest loginRequest,
+      HttpServletResponse response) {
+
+    adminMemberService.login(loginRequest.username(), loginRequest.password());
+
+    String accessToken = jwtTokenUtils.generateJwtToken(loginRequest.username(),
+        TokenDuration.ACCESS_TOKEN_DURATION_ADMIN.getDuration());
+
+    setAccessTokenInCookie(accessToken, response);
+
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/logout")
+  public ResponseEntity<Void> logout(@CookieValue(name = "accessToken") Cookie cookie,
+      HttpServletResponse response) {
+    setTokenMaxAgeZero(cookie, response);
+    return ResponseEntity.ok().build();
+  }
+
+  private static void setTokenMaxAgeZero(Cookie cookie, HttpServletResponse response) {
+    cookie.setValue("");
+    cookie.setMaxAge(0);
+    response.addCookie(cookie);
+  }
+
+
+  private void setAccessTokenInCookie(String accessToken, HttpServletResponse response) {
+    Cookie accessTokenCookie = new Cookie("accessToken", "Bearer " + accessToken);
+    accessTokenCookie.setMaxAge(TokenDuration.ACCESS_TOKEN_DURATION_ADMIN.getDurationInSecond());
+    accessTokenCookie.setHttpOnly(true);
+    response.addCookie(accessTokenCookie);
+  }
+
+}
