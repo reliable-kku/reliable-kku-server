cf53380651cd6d3b406024b9c064bc4e0e0182d3
wan2daaa <wan2daaa@gmail.com>
2023-11-01 02:11:47 +0900

    fix: H2 데이터베이스 확인하기 위해 수정

--- src/main/java/com/deundeunhaku/reliablekkuserver/common/security/SpringSecurityConfiguration.java
+++ src/main/java/com/deundeunhaku/reliablekkuserver/common/security/SpringSecurityConfiguration.java
@@ -3,9 +3,7 @@ package com.deundeunhaku.reliablekkuserver.common.security;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 import com.deundeunhaku.reliablekkuserver.common.security.filter.JwtAuthenticationFilter;
-import com.deundeunhaku.reliablekkuserver.jwt.util.JwtTokenUtils;
 import com.deundeunhaku.reliablekkuserver.member.constant.Role;
-import com.deundeunhaku.reliablekkuserver.member.repository.MemberRepository;
 import com.deundeunhaku.reliablekkuserver.member.service.MemberDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -17,17 +15,12 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @RequiredArgsConstructor
 @Configuration
@@ -59,8 +52,13 @@ public class SpringSecurityConfiguration {
                 .requestMatchers(antMatcher("/api/v1/**")).hasRole(Role.USER.name())
                 .anyRequest().authenticated()
         )
-        .sessionManagement((session) -> session
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        .headers(headers ->
+            headers
+                .frameOptions(FrameOptionsConfig::sameOrigin)
+        );
+
+//        .sessionManagement((session) -> session
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     http
         .authenticationProvider(authenticationProvider()).addFilterBefore(
