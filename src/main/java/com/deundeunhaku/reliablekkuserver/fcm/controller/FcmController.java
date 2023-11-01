60a1a3f61332bac34744a27380a1339e3fa991bd
wan2daaa <wan2daaa@gmail.com>
2023-11-01 18:58:30 +0900

    feat: member의 fcm token 받는 api 구현

---
+++ src/main/java/com/deundeunhaku/reliablekkuserver/fcm/controller/FcmController.java
@@ -0,0 +1,29 @@
+package com.deundeunhaku.reliablekkuserver.fcm.controller;
+
+import com.deundeunhaku.reliablekkuserver.fcm.dto.FcmTokenRequest;
+import com.deundeunhaku.reliablekkuserver.member.domain.Member;
+import com.deundeunhaku.reliablekkuserver.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/fcm")
+public class FcmController {
+
+  private final MemberService memberService;
+
+  @PostMapping
+  public ResponseEntity<Void> updateFcmToken(@AuthenticationPrincipal Member member,
+      @RequestBody FcmTokenRequest request) {
+    memberService.updateFcmToken(member, request.token());
+
+    return ResponseEntity.ok().build();
+  }
+
+}
---
+++ src/test/java/com/deundeunhaku/reliablekkuserver/fcm/controller/FcmControllerTest.java
@@ -0,0 +1,45 @@
+package com.deundeunhaku.reliablekkuserver.fcm.controller;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.deundeunhaku.reliablekkuserver.BaseControllerTest;
+import com.deundeunhaku.reliablekkuserver.fcm.dto.FcmTokenRequest;
+import com.deundeunhaku.reliablekkuserver.member.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.web.servlet.ResultActions;
+
+class FcmControllerTest extends BaseControllerTest {
+
+  @MockBean
+  private MemberService memberService;
+
+  @Test
+  void 멤버의_firebaseToken을_저장한다() throws Exception {
+      //given
+    FcmTokenRequest request = FcmTokenRequest.of("firebaseToken");
+
+    //when
+    ResultActions resultActions = mockMvc.perform(post(API + "/fcm")
+            .contentType(APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+        .andDo(print());
+
+    //then
+    resultActions
+        .andExpect(status().isOk())
+        .andDo(document("fcm/update",
+            requestFields(
+                fieldWithPath("token").description("firebase token")
+            )));
+
+  }
+
+}
