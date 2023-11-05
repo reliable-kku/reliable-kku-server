c97f22ee07abd66b0ae2153a0c12bb77f10e1ec1
MJ <rlaalswls102@naver.com>
2023-11-05 23:59:39 +0900

    fix: git

--- src/main/java/com/deundeunhaku/reliablekkuserver/payment/dto/PaymentCancelDataResponse.java
+++ src/main/java/com/deundeunhaku/reliablekkuserver/payment/dto/PaymentCancelDataResponse.java
@@ -7,12 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public class PaymentCancelDataResponse {
 
-  private Number cancelAmount;
+  private Integer cancelAmount;
   private String cancelReason;
-  private Number taxFreeAmount;
+  private Integer taxFreeAmount;
   private Integer taxExemptionAmount;
-  private Number refundableAmount;
-  private Number easyPayDiscountAmount;
+  private Integer refundableAmount;
+  private Integer easyPayDiscountAmount;
   private String canceledAt;
   private String transactionKey;
 }
