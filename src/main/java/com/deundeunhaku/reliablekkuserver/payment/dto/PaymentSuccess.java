896780f806855d056e132e33e04fbbff0217d584
wan2daaa <wan2daaa@gmail.com>
2023-11-06 03:11:59 +0900

    fix: toss pg response 수정

--- src/main/java/com/deundeunhaku/reliablekkuserver/payment/dto/PaymentSuccess.java
+++ src/main/java/com/deundeunhaku/reliablekkuserver/payment/dto/PaymentSuccess.java
@@ -1,6 +1,7 @@
 package com.deundeunhaku.reliablekkuserver.payment.dto;
 
 import com.deundeunhaku.reliablekkuserver.payment.constants.PayType;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,20 +11,71 @@ public class PaymentSuccess {
 
   private String mId;// 가맹점 Id -> tosspayments
   private String version; // Payment 객체 응답 버전
+  private String lastTransactionKey;
   private String paymentKey;
   private String orderId;
   private String orderName;
+  private String currency;
   private String method; // 결제 수단
-  private String totalAmount;
-  private String balanceAmount;
-  private String suppliedAmount;
-  private String vat; // 부가가치세
-  private String status; // 결제 처리 상태
-  private String requestedAt;
-  private String approvedAt;
-  private String useEscrow; // false
-  private String cultureExpense; // false
-  private PaymentSuccessCard card; // 결제 카드 정보 (아래 자세한 정보 있음)
-  private PayType type; // 결제
+  private String status;
+  private LocalDateTime requestedAt;
+  private LocalDateTime approvedAt;
+  private Boolean useEscrow;
+  private Boolean cultureExpense;
+  private PaymentCardResponse card;
+  private String virtualAccount;
+  private String transfer;
+  private String mobilePhone;
+  private String giftCertificate;
+  private String foreignEasyPay;
+  private String cashReceipt;
+  private String cashReceipts;
+  private Receipt receipt;
+  private Checkout checkout;
+  private String discount;
+  private String cancels;
+  private String secret;
+  private PayType type;
+  private String easyPay;
+  private String country;
+  private String failure;
+  private Integer totalAmount;
+  private Integer balanceAmount;
+  private Integer suppliedAmount;
+  private Integer vat;
+  private Integer taxFreeAmount;
+  private Integer taxExemptionAmount;
 
+}
+
+@AllArgsConstructor
+@Getter
+class PaymentCardResponse {
+
+  private String amount;
+  private String issuerCode;
+  private String acquirerCode;
+  private String number;
+  private Integer installmentPlanMonths;
+  private Boolean isInterestFree;
+  private String interestPayer;
+  private String approveNo;
+  private Boolean useCardPoint;
+  private String cardType;
+  private String ownerType;
+  private String acquireStatus;
+}
+
+@AllArgsConstructor
+@Getter
+class Receipt {
+
+  private String url;
+}
+
+@AllArgsConstructor
+@Getter
+class Checkout {
+
+  private String url;
 }
