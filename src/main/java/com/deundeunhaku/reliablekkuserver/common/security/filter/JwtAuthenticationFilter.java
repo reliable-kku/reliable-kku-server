9bcfa3b51b6460599ff4adc74d4c43aae4de63cd
wan2daaa <wan2daaa@gmail.com>
2023-11-13 22:41:31 +0900

    fix: 요청 api url 전송 및 requstMatcher 수정

--- src/main/java/com/deundeunhaku/reliablekkuserver/common/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/deundeunhaku/reliablekkuserver/common/security/filter/JwtAuthenticationFilter.java
@@ -31,33 +31,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   private final JwtTokenUtils jwtTokenUtils;
   private final MemberDetailsService memberDetailsService;
 
-  private final List<String> WHITELIST_URL =
-      List.of(
-          "/api/v1/auth/admin/login",
-          "/api/v1/auth/admin/logout",
-          "/api/v1/auth/login",
-          "/api/v1/find-password/phone-number/certification-number",
-          "/api/v1/find-password",
-          "/api/v1/register/phone-number/duplicate",
-          "/api/v1/register/phone-number/certification-number",
-          "/api/v1/register",
-          "/api/v1/order/sse/connect",
-          "/api/v1/admin/order/sse/connect",
-          "/docs/index.html",
-          "/api/v1/token/valid",
-          "/api/v1/token/update"
-      );
-
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
 
     String requestURI = request.getRequestURI();
 
-    if (WHITELIST_URL.contains(requestURI)) {
-      filterChain.doFilter(request, response);
-      return;
-    }
     log.info("requestURI: {}", requestURI);
 
     if (request.getHeader(AUTHORIZATION) == null || request.getHeader(AUTHORIZATION).isEmpty()) {
